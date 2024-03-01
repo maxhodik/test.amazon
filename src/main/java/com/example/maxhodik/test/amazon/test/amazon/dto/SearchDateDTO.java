f4c7c07778ea09b0d25862e14940c1debaec0070
m-god <m-godik@ukr.net>
2024-03-01 20:26:49 +0200

    fix bug DataDto

--- src/main/java/com/example/maxhodik/test/amazon/test/amazon/dto/SearchDateDTO.java
+++ src/main/java/com/example/maxhodik/test/amazon/test/amazon/dto/SearchDateDTO.java
@@ -4,9 +4,11 @@ import com.example.maxhodik.test.amazon.test.amazon.util.annotation.ValidDate;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SearchDateDTO {
     @NotNull
     @ValidDate
